55b255f0464927b034ebbc8899423481a5aa48c8
sochetanie <allagorik2901@gmail.com>
2017-06-06 16:20:35 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+    var counter = 0
+
+    function addBump() {
+        counter += 1
+    }
+
+    function getBumps() {
+        return counter
+    }
+
+    return {
+        addBump,
+        getBumps
+    }
+
+}
+
+
+function createAnimal (animalType) {
+    function xXx (deadlyDevice) {
+        return {
+            animalType,
+            deadlyDevice
+        }
+    }
+    return xXx
+}
+
+
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
